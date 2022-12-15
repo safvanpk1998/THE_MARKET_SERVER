195db4bb897137e7aac0f97d84669c449a0ed033
safvanpk1998 <68804604+safvanpk1998@users.noreply.github.com>
2022-12-15 14:19:16 +0530

    core option

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app.use(cookieParser())
 
 const cors = require('cors');
 const corsOptions ={
-    origin:'http://localhost:3000', 
+    origin:'https://the-market.onrender.com', 
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
