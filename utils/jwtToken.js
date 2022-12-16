e8c1ac815f425f62d13755e1db62f76a59594396
safvanpk1998 <68804604+safvanpk1998@users.noreply.github.com>
2022-12-16 10:51:14 +0530

    change

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -10,12 +10,16 @@ const sendToken=(user,statusCode,res)=>{
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         )
         , secure:process.env.NODE_ENV !== "PRODUCTION"
-        ,httpOnly:true
+        ,httpOnly:true,
+        sameSite: 'none',
+    
+        
     };
     res.status(statusCode).cookie('token',token,option).json({
         success:true,
         user,
         token
+        
     })
 };
 module.exports=sendToken
