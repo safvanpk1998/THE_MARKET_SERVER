7842d4b7f08e6be405cdaf894eb2f28863d94bcf
safvanpk1998 <68804604+safvanpk1998@users.noreply.github.com>
2022-12-21 16:25:53 +0530

    getkey

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -15,7 +15,7 @@ const { isAuthenticatedUser, authorizedRoles } = require("../middleware/auth");
 const router = express.Router();
 
 router.route("/order/new").post(isAuthenticatedUser, createNewOrder);
-router.route("/api/v1/getkey").get(isAuthenticatedUser, getKey);
+router.route("/getkey").get(isAuthenticatedUser, getKey);
 router.route("/order/razorpayPayment").post(isAuthenticatedUser, razorpayPayment);
 router.route("/order/myorders").get(isAuthenticatedUser, myOrder);
 router
